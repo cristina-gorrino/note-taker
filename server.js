b9076a6ea8513e231dccf39da1b343ad159dc58e
cgorrino <cgorrino03@gmail.com>
2021-07-17 11:18:10 -0700

    Adding compression

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
-
+var compression = require('compression')
 const express = require('express');
 
 // EXPRESS CONFIGURATION
@@ -12,10 +12,12 @@ const app = express();
 // Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 8080;
 
+app.use(compression());
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
 
 // ROUTER
 // The below points our server to a series of "route" files.
